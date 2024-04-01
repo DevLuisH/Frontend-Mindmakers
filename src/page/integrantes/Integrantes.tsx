1593d2dc23b6824791426f7765b1818d15eb6363
Sabrina da Motta Café <107332854+sabrinacafe@users.noreply.github.com>
2024-04-01 18:29:58 -0300

    Update Integrantes.tsx

--- src/page/integrantes/Integrantes.tsx
+++ src/page/integrantes/Integrantes.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Integrantes: React.FC = () => {
   return (
     <div>
-      <h1>Luis Henrique RM:552692;<br />Sabrina Café;<br /> João
+      <h1>Luis Henrique RM:552692;<br />Sabrina Café RM: 553568;<br /> João
       </h1>
     </div>
   );
