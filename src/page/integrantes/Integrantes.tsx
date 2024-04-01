c3a42e0c7898f89d1f3663a8a89d2b67f79af392
Luis Henrique Oliveira <139643431+DevLuisH@users.noreply.github.com>
2024-04-01 18:24:21 -0300

    Update Integrantes.tsx

--- src/page/integrantes/Integrantes.tsx
+++ src/page/integrantes/Integrantes.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Integrantes: React.FC = () => {
   return (
     <div>
-      <h1>Luis Henrique 552692;<br />Sabrina Café;<br /> João
+      <h1>Luis Henrique RM:552692;<br />Sabrina Café;<br /> João
       </h1>
     </div>
   );
